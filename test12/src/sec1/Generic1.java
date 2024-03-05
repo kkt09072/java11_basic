964854bbd5d04c6e267d4474cd2e1b9e0f0180a3
kkt09072 <ksb450424@naver.com>
2024-03-05 17:48:59 +0900

    java11 generic & collection framework

--- test12/src/sec1/Generic1.java
+++ test12/src/sec1/Generic1.java
@@ -1,5 +1,7 @@
 package sec1;
-//제네릭(Generic)이란? 데이터와 관계없이 아무거나 모두 받아서 처리하는 클래스
+//제네릭(Generic)이란? 데이터와 관계없이 아무거나 모두 받아서 처리하는 클래스 -> 자바 1.5이상 지원
+//자주 사용하는 타입 인자 : T(Type), E(Element), K(Key), V(Value), N(Number), R(Result)
+//<> : 다이아몬드 연산자
 public class Generic1<T> {
 	private T field1;
 	public Generic1() {}
